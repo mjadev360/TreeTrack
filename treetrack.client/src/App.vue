26f5c9b9b74e76c7e5f972ff9d392c751010e1bd
Matthew <mattlokk@gmail.com>
2026-07-01 14:00:59 -0700

    favicon and site title

--- treetrack.client/src/App.vue
+++ treetrack.client/src/App.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
+import { useProjectStore } from '@/stores/projectStore'
 import MainLayout from './layouts/MainLayout.vue'
 import AuthLayout from './layouts/AuthLayout.vue'
 import IssuesLayout from './layouts/IssuesLayout.vue'
 
+const DEFAULT_TITLE = 'TreeTrack'
+
 const route = useRoute()
+const projectStore = useProjectStore()
+
+watch(
+  () => [route.name, projectStore.currentProject?.name] as const,
+  ([routeName, projectName]) => {
+    document.title =
+      routeName === 'ProjectIssues' && projectName
+        ? `TT - ${projectName}`
+        : DEFAULT_TITLE
+  },
+  { immediate: true }
+)
 
 const layout = computed(() => route.meta.layout)
 
